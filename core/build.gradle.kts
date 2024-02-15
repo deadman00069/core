173af2f3ad6474259e4700e73b1b841922cd5850
deadman00069 <67422898+deadman00069@users.noreply.github.com>
2024-02-15 20:52:51 +0530

    init

---
+++ core/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    `android-library`
+    `kotlin-android`
+    id("maven-publish")
+}
+
+apply<MainGradlePlugin>()
+
+android {
+    namespace = "com.foss.core"
+
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+dependencies {
+
+    //Need below three in every module
+    Dependencies.coreKtx
+    Dependencies.appCompat
+    hilt()
+    hiltTesting()
+    retrofit()
+    timber()
+    dataStore()
+    moshi()
+
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "com.github.deadman00069"
+            artifactId = "core"
+            version = "2.0.4"
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
+}
